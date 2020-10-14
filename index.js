97336c9931277e8df9855c1016cb9541f2f87db6
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2020-10-15 00:08:18 +0300

    Finish task

--- index.js
+++ index.js
@@ -11,29 +11,48 @@ let room1 = document.querySelector('#room1');
 let room2 = document.querySelector('#room1');
 let room3 = document.querySelector('#room1');
 let submitBtn = document.querySelector('#submit');
+let message = document.querySelector('.message');
+let spin = document.querySelector('.spinn');
 let photos = document.querySelector('.choosed-number-type');
+let faq = document.querySelector('#faq');
+let about = document.querySelector('#about-us');
 const clasForRemoveElement = 'd-lg-none  d-md-none d-sm-none d-none';
+let hotelBookName;
 
 const bookNumber = (e) => {
   let allText = e.target.parentNode.innerText;
   let hotelName = allText.split('\n')[0];
+  hotelBookName = hotelName;
   let pathToImage =
     e.target.parentNode.parentNode.firstElementChild.attributes[1].value;
   showTable(hotelName, pathToImage);
 };
 
 const showMainContent = () => {
+  window.scrollTo(0, 500);
   main.setAttribute('class', 'main d-flex align-items-center flex-column mb-5');
+  faq.setAttribute('class', 'faq d-flex align-items-center flex-column mb-4');
+  about.setAttribute(
+    'class',
+    'about-us d-flex align-items-center flex-column mb-4'
+  );
   hotels.setAttribute(
     'class',
     'hotels-main d-flex align-items-center flex-column mb-4'
   );
   photos.setAttribute('class', clasForRemoveElement);
+  message.setAttribute('class', clasForRemoveElement);
+  submitBtn.style.display = 'block';
+  spin.setAttribute(
+    'class',
+    'd-lg-none d-md-none d-sm-none d-none spinn d-flex justify-content-center mb-1'
+  );
   bookingRooms.setAttribute('class', clasForRemoveElement);
 };
 
 function showRoomsPhotos() {
   photos.setAttribute('class', 'choosed-number-type');
+  window.scrollTo(0, 1200);
 }
 const row1Photos = (e) => {
   showRoomsPhotos();
@@ -54,11 +73,23 @@ const row3Photos = (e) => {
   room3.setAttribute('src', 'images/sroom3.jpg');
 };
 
+const onSubmitBtnClick = () => {
+  submitBtn.style.display = 'none';
+  spin.setAttribute('class', ' spinn d-flex justify-content-center mb-1');
+  setTimeout(() => spin.setAttribute('class', clasForRemoveElement), 3000);
+  setTimeout(() => message.setAttribute('class', 'message jumbotron'), 3100);
+};
+
 function showTable(hotelName, pathToImage) {
+  window.scrollTo(0, 450);
   let choosedHotelName = document.querySelector('#choose-hotel-name');
+  let choosedHotelHeading = document.querySelector('#choosedHotelName');
   let img = document.querySelector('#choosed-hotel-image');
   main.setAttribute('class', clasForRemoveElement);
   hotels.setAttribute('class', clasForRemoveElement);
+  faq.setAttribute('class', clasForRemoveElement);
+  about.setAttribute('class', clasForRemoveElement);
+  choosedHotelHeading.innerHTML = hotelBookName;
   bookingRooms.setAttribute(
     'class',
     'booking-rooms d-flex align-items-center flex-column mb-4'
@@ -68,6 +99,7 @@ function showTable(hotelName, pathToImage) {
 }
 
 backToHotelBtn.addEventListener('click', showMainContent);
+submitBtn.addEventListener('click', onSubmitBtnClick);
 row1.addEventListener('click', row1Photos);
 row2.addEventListener('click', row2Photos);
 row3.addEventListener('click', row3Photos);
